cfdaa8a245b37015beda6d41e57d3308c7ad549e
sbychin <sbychin@plexteq.com>
2020-10-28 21:33:42 +0200

    Added factory for ReposVC

--- Headway_TestTask/Scenes/Repos/ReposViewController.swift
+++ Headway_TestTask/Scenes/Repos/ReposViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-class ReposViewController: UIViewController {
+final class ReposViewController: DisposeViewController {
+    @IBOutlet private (set) var tableView: UITableView!
+    @IBOutlet private (set) var searchBar: UISearchBar!
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+extension ReposViewController: StaticFactory {
+    enum Factory {
+        static var `default`: ReposViewController {
+            let reposVC = R.storyboard.main.reposViewController()!
+            
+            return reposVC
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
