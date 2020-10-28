00c8b643531d4bf4e26d687ceb9498b9a7ee4a53
sbychin <sbychin@plexteq.com>
2020-10-28 21:54:10 +0200

    Added RepoTableViewCell

---
+++ Headway_TestTask/Scenes/Repos/Cell/RepoTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RepoTableViewCell.swift
+//  Headway_TestTask
+//
+//  Created by Serhii Bychin on 28.10.2020.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+    @IBOutlet private (set) var repoNameLabel: UILabel!
+    @IBOutlet private (set) var repoImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configure(withImageUrl url: String?,
+                   name: String) {
+        if let url = url {
+            Nuke.loadImage(with: URL(string: url)!, into: repoImageView)
+        }
+        repoNameLabel.text = name
+    }
+}
